c2e116623d6f6fc567ba15c222885acc64b9816e
sangyoo91 <sangyookim@Sangs-iMac.local>
2015-02-25 18:41:14 +0900

    Added Packages, Head, and Signin/Register Modal(non functional)

---
+++ client/template/includes/modal/signin_modal.js
@@ -0,0 +1,16 @@
+Template.signinModal.rendered = function(){
+
+
+}
+
+Template.signinModal.events({
+
+	// 'click #signinTab': function(){
+	// 	$(".modal-dialog").animate({"height":"510px"},600,'linear');
+	// },
+
+	// 'click #registerTab': function(){
+	// 	$(".modal-dialog").animate({"height":"800px"},600,'linear');
+	// }
+
+})
