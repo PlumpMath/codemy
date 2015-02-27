14e6429fa4844908e25c8302e16e9d0b97c3e0d9
sangyoo91 <sangyookim@Sangs-iMac.local>
2015-02-28 03:25:19 +0900

    Added Navbar center home link. Minor Fixes.

--- client/templates/search/search.js
+++ client/templates/search/search.js
@@ -1,18 +1,18 @@
 Template.search.events({
-    'click #linkToPost':function () {
-      Overlay.hide();
-    } 
+	'click #linkToPost':function () {
+	  Overlay.hide();
+	} 
 });
 
 Template.search.rendered = function(){
 	
-	$('.overlay').keydown(function(e) {
-    	// ESCAPE key pressed
-    	if (e.keyCode == 27) {
-		   Overlay.hide();		
-    	}
+	$('.overlay').keyup(function(e) {
+		// ESCAPE key pressed
+		if (e.keyCode == 27) {
+	   Overlay.hide();		
+		}
 	});
 
-    $("input:text:visible:first").focus();
+	$("input:text:visible:first").focus();
 	
 };
